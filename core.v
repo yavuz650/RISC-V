de1cb501a0a5789973dce275d4ed6c12e1868c93
Yavuz Selim <0yavuz0@gmail.com>
2020-11-12 13:43:39 +0300

    fixed white spaces

--- core.v
+++ core.v
@@ -1,15 +1,15 @@
 `timescale 1ns/10ps
 
 module core(input reset_i,
-	    	input clk_i,
-	    	input [31:0] instr_i,
-	    	input [31:0] data_i,
+            input clk_i,
+            input [31:0] instr_i,
+            input [31:0] data_i,
 	    	
-	    	output [3:0] wmask0_o,
-	    	output 		 wen0_o,
-	    	output [10:0] instr_addr_o,
-	    	output [10:0] data_addr_o,
-	    	output [31:0] data_o); //active-low reset. all write_enable signals are also active-low.
+            output [3:0]  wmask0_o,
+            output        wen0_o,
+            output [10:0] instr_addr_o,
+            output [10:0] data_addr_o,
+            output [31:0] data_o); //active-low reset. all write_enable signals are also active-low.
 				  
 
 //IF signals------------------------------------------------------------
@@ -19,7 +19,7 @@ wire        muxpc_ctrl; //pc-mux control signal. it is also controls the flush m
 reg [31:0]  pc_o; //pc out
 wire [10:0] mux_addr_o, mux_load_o, mux_addr_o2;
 wire [31:0] instr_IF;
-wire 		IFID_preg_wen; //IF/ID pipeline register write enable signal.
+wire        IFID_preg_wen; //IF/ID pipeline register write enable signal.
 //pipeline register
 reg [31:0] IFID_preg_instr;
 reg [31:0] IFID_preg_pc;
@@ -43,24 +43,24 @@ reg [31:0] IDEX_preg_imm;
 reg [4:0]  IDEX_preg_rd, IDEX_preg_rs2, IDEX_preg_rs1;
 reg [31:0] IDEX_preg_data2, IDEX_preg_data1;
 reg [31:0] IDEX_preg_pc;
-reg	[8:0]  IDEX_preg_ex;
+reg [8:0]  IDEX_preg_ex;
 reg [5:0]  IDEX_preg_mem;
 reg 	   IDEX_preg_wb;
 
 reg [31:0] register_bank [31:0]; //32x32 register file
 //----------------------------------------------------------------------
 //EX signals------------------------------------------------------------
-wire 		wb_EX;
-wire [5:0] 	mem_EX;
-wire [8:0] 	ex_EX;
+wire        wb_EX;
+wire [5:0]  mem_EX;
+wire [8:0]  ex_EX;
 wire [31:0] pc_EX, data1_EX, data2_EX, imm_EX;
-wire [4:0] 	rs1_EX, rs2_EX, rd_EX;
-wire 		mux1_ctrl_EX, mux3_ctrl_EX, mux5_ctrl_EX;
-wire [1:0] 	mux2_ctrl_EX, mux4_ctrl_EX;
+wire [4:0]  rs1_EX, rs2_EX, rd_EX;
+wire        mux1_ctrl_EX, mux3_ctrl_EX, mux5_ctrl_EX;
+wire [1:0]  mux2_ctrl_EX, mux4_ctrl_EX;
 wire [31:0] mux1_o_EX, mux2_o_EX, mux3_o_EX, mux4_o_EX, mux5_o_EX;
-wire [3:0] 	alu_ctrl;
+wire [3:0]  alu_ctrl;
 wire [31:0] aluout_EX;
-wire 		J, B, L; //jump, branch, load
+wire        J, B, L; //jump, branch, load
 //pipeline register
 reg [31:0] EXMEM_preg_imm;
 reg [4:0]  EXMEM_preg_rd;
@@ -70,17 +70,17 @@ reg [5:0]  EXMEM_preg_mem;
 reg        EXMEM_preg_wb;
 //----------------------------------------------------------------------
 //MEM signals-----------------------------------------------------------
-wire 		wb_MEM;
-wire [5:0] 	mem_MEM;
+wire        wb_MEM;
+wire [5:0]  mem_MEM;
 wire [31:0] aluout_MEM, data2_MEM;
-wire [4:0] 	rd_MEM;
+wire [4:0]  rd_MEM;
 wire [31:0] imm_MEM;
 wire [31:0] memout_MEM;
 //internal nets
-wire 	   wen_MEM;
-wire	   wen_MEM_r;
+wire       wen_MEM;
+wire       wen_MEM_r;
 wire [1:0] ls_length, ls_length_r;
-wire 	   l_sign;
+wire       l_sign;
 wire [1:0] mux_ctrl_MEM;
 wire [3:0] wmask0;
 //pipeline register
@@ -90,8 +90,8 @@ reg        MEMWB_preg_wb;
 reg [37:0] MEMWB_preg;
 //----------------------------------------------------------------------
 //WB signals------------------------------------------------------------
-wire [4:0] 	rd_WB;
-wire 		wb_WB;
+wire [4:0]  rd_WB;
+wire        wb_WB;
 wire [31:0] memout_WB;
 //----------------------------------------------------------------------
 
@@ -135,18 +135,18 @@ end
 
 //ID STAGE---------------------------------------------------------------------------------
 //assign nets
-assign rs1_ID 		= IFID_preg_instr[19:15];
-assign rs2_ID		= IFID_preg_instr[24:20];
+assign rs1_ID       = IFID_preg_instr[19:15];
+assign rs2_ID       = IFID_preg_instr[24:20];
 assign rd_ID        = IFID_preg_instr[11:7];
-assign pc_ID 		= IFID_preg_pc;
+assign pc_ID        = IFID_preg_pc;
 assign imm_dec_i    = IFID_preg_instr[31:2];
 assign ctrl_unit_i  = {IFID_preg_instr[31:25], IFID_preg_instr[14:12], IFID_preg_instr[6:2]};
-assign mux1_o_ID	= mux_ctrl_ID ? 1'b1 : ctrl_unit_wb_o;
-assign mux2_o_ID	= mux_ctrl_ID ? 6'b1 : ctrl_unit_mem_o;
-assign mux3_o_ID	= mux_ctrl_ID ? 9'b0 : ctrl_unit_ex_o;
+assign mux1_o_ID    = mux_ctrl_ID ? 1'b1 : ctrl_unit_wb_o;
+assign mux2_o_ID    = mux_ctrl_ID ? 6'b1 : ctrl_unit_mem_o;
+assign mux3_o_ID    = mux_ctrl_ID ? 9'b0 : ctrl_unit_ex_o;
 
-control_unit  		  CTRL_UNIT 	(.control_i(ctrl_unit_i), .WB_o(ctrl_unit_wb_o), .MEM_o(ctrl_unit_mem_o), .EX_o(ctrl_unit_ex_o));
-imm_decoder   		  IMM_DEC   	(.instr_in(imm_dec_i), .imm_out(imm_dec_o));
+control_unit    CTRL_UNIT 	(.control_i(ctrl_unit_i), .WB_o(ctrl_unit_wb_o), .MEM_o(ctrl_unit_mem_o), .EX_o(ctrl_unit_ex_o));
+imm_decoder     IMM_DEC   	(.instr_in(imm_dec_i), .imm_out(imm_dec_o));
 
 integer i;
 always @(negedge clk_i or negedge reset_i) //write to register file
@@ -213,10 +213,10 @@ assign imm_EX   = IDEX_preg_imm;
 assign mux1_ctrl_EX = ex_EX[4];
 assign mux3_ctrl_EX = ex_EX[5];
 assign mux5_ctrl_EX = ex_EX[6];
-assign alu_ctrl  	= ex_EX[3:0];
-assign J 		 	= ex_EX[7]; //jump
-assign B 		 	= ex_EX[8]; //branch
-assign L 		 	= (!wb_EX & mem_EX[5:4] == 2'b1) ? 1'b1 : 1'b0; //load
+assign alu_ctrl     = ex_EX[3:0];
+assign J            = ex_EX[7]; //jump
+assign B            = ex_EX[8]; //branch
+assign L            = (!wb_EX & mem_EX[5:4] == 2'b1) ? 1'b1 : 1'b0; //load
 //muxes
 assign mux1_o_EX = mux1_ctrl_EX ? pc_EX : mux2_o_EX;
 assign mux2_o_EX = mux2_ctrl_EX == 2'b10 ? aluout_MEM
@@ -229,9 +229,9 @@ assign mux4_o_EX = mux4_ctrl_EX == 2'b10 ? data2_EX
 assign mux5_o_EX = mux5_ctrl_EX ? pc_EX	 : mux2_o_EX;
 
 
-assign wen0_o	 	= mem_EX[0];
-assign ls_length_r 	= mem_EX[2:1];
-assign wmask0_o		= ls_length_r == 2'b0 ? 4'b0001
+assign wen0_o       = mem_EX[0];
+assign ls_length_r  = mem_EX[2:1];
+assign wmask0_o     = ls_length_r == 2'b0 ? 4'b0001
 					: ls_length_r == 2'b1 ? 4'b0011
 					: 4'b1111;
 //instantiate the forwarding unit.
@@ -273,9 +273,9 @@ assign data2_MEM  = EXMEM_preg_data2;
 assign rd_MEM 	  = EXMEM_preg_rd;
 assign imm_MEM 	  = EXMEM_preg_imm;
 //assign nets
-assign wen_MEM	    = mem_MEM[0];
-assign ls_length 	= mem_MEM[2:1];
-assign l_sign	    = mem_MEM[3];
+assign wen_MEM      = mem_MEM[0];
+assign ls_length    = mem_MEM[2:1];
+assign l_sign       = mem_MEM[3];
 assign mux_ctrl_MEM = mem_MEM[5:4];
 
 always @(posedge clk_i or negedge reset_i)
--- top_module.v
+++ top_module.v
@@ -1,10 +1,10 @@
 `timescale 1ns/10ps
 
 module top_module(input clk_i,
-				  input reset_i,
-				  input wen_i,
-				  input [10:0] addr_i,
-				  input [31:0] instr_i);
+                  input reset_i,
+                  input wen_i,
+                  input [10:0] addr_i,
+                  input [31:0] instr_i);
 				  
 wire [3:0] wmask0, wmask1;
 wire wen0;
@@ -17,6 +17,6 @@ assign mux_addr_o = wen_i ? instr_addr_o : addr_i;
 core core0(.clk_i(clk_i), .reset_i(reset_i), .instr_i(mem_instr_o), .data_i(mem_data_o), .data_o(data_o), .wmask0_o(wmask0), .wen0_o(wen0), .instr_addr_o(instr_addr_o), .data_addr_o(data_addr_o) );
 
 sram_1rw1r_32_512_8_freepdk45 memory(.clk0(clk_i), .csb0(!reset_i), .web0(wen0), .wmask0(wmask0), .addr0(data_addr_o[10:2]), .din0(data_o), .dout0(mem_data_o), //port 0 is for data memory
-	 								 .clk1(clk_i), .csb1(1'b0), .web1(wen_i), .wmask1(4'b1111), .addr1(mux_addr_o[10:2]), .din1(instr_i), .dout1(mem_instr_o)); //port 1 is for instruction memory
+                                     .clk1(clk_i), .csb1(1'b0), .web1(wen_i), .wmask1(4'b1111), .addr1(mux_addr_o[10:2]), .din1(instr_i), .dout1(mem_instr_o)); //port 1 is for instruction memory
 
 endmodule
