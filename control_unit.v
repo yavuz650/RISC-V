1e7bfeff00467f3ad8e040c02cd38821fa4e0062
Yavuz Selim <0yavuz0@gmail.com>
2020-11-12 03:00:25 +0300

    Add files via upload

--- control_unit.v
+++ control_unit.v
@@ -1,17 +1,19 @@
 `timescale 1ns/10ps
 
-`define WB 		 fields_out[15]
-`define MEM_MUX  fields_out[14:13]
-`define MEM_SIGN fields_out[12]
-`define MEM_LEN  fields_out[11:10]
-`define MEM_WEN  fields_out[9]
-`define B 		 fields_out[8]
-`define J 		 fields_out[7]
-`define EX_MUX3  fields_out[6]
-`define EX_MUX2  fields_out[5]
-`define EX_MUX1  fields_out[4]
-`define ALU 	 fields_out[3:0]
+//output fields
+`define WB 		 WB_o
+`define MEM_MUX  MEM_o[5:4]
+`define MEM_SIGN MEM_o[3]
+`define MEM_LEN  MEM_o[2:1]
+`define MEM_WEN  MEM_o[0]
+`define B 		 EX_o[8]
+`define J 		 EX_o[7]
+`define EX_MUX3  EX_o[6]
+`define EX_MUX2  EX_o[5]
+`define EX_MUX1  EX_o[4]
+`define ALU 	 EX_o[3:0]
 
+//mux control signals
 `define data1_EX 1'b0
 `define data2_EX 1'b0
 `define imm_EX   1'b1
@@ -21,18 +23,19 @@
 `define memout_MEM 2'd1
 `define imm_MEM    2'd2
 
-module control_unit(	input [14:0] control_in, //instr[31:25|14:12|6:2]
-			
-			output reg [15:0] fields_out); // WB|MEM|EX
+module control_unit(input [14:0] control_i, //instr[31:25|14:12|6:2]
+					output reg WB_o,
+					output reg [5:0] MEM_o,
+					output reg [8:0] EX_o);
 			
 always @*
 begin
-	casez(control_in[4:0])
+	casez(control_i[4:0])
 	
 		//BEQ, BNE, BLT, BGE, BLTU, BGEU
 		5'b11000: begin 
 			`WB = 1'b1; `MEM_MUX = `aluout_MEM; `MEM_SIGN = 1'b0; `MEM_LEN = 2'b0; `MEM_WEN = 1'b1; `B = 1'b1; `J = 1'b0; `EX_MUX3 = `pc_EX; `EX_MUX2 = `data2_EX; `EX_MUX1 = `data1_EX;
-			case(control_in[7:5])
+			case(control_i[7:5])
 				3'b000: `ALU = 4'b1010; //BEQ
 				3'b001: `ALU = 4'b1011; //BNE
 				3'b100: `ALU = 4'b0110; //BLT
@@ -51,7 +54,7 @@ begin
 		//JAL, JALR
 		5'b110?1: begin 
 			`WB = 1'b0; `MEM_MUX = `aluout_MEM; `MEM_SIGN = 1'b0; `MEM_LEN = 2'b0; `MEM_WEN = 1'b1; `B = 1'b0; `J = 1'b1; `EX_MUX2 = `data2_EX; `EX_MUX1 = `pc_EX; `ALU = 4'b1110;
-			case(control_in[1])
+			case(control_i[1])
 				1'b1: `EX_MUX3 = `pc_EX;    //JAL
 				1'b0: `EX_MUX3 = `data1_EX; //JALR
 			endcase
@@ -60,7 +63,7 @@ begin
 		//LB, LH, LW, LBU, LHU
 		5'b00000: begin 
 			`WB = 1'b0; `MEM_MUX = `memout_MEM; `MEM_WEN = 1'b1; `B = 1'b0; `J = 1'b0; `EX_MUX3 = `data1_EX; `EX_MUX2 = `imm_EX; `EX_MUX1 = `data1_EX; `ALU = 4'b0000;
-			case(control_in[7:5])
+			case(control_i[7:5])
 				3'b000: begin `MEM_SIGN = 1'b1; `MEM_LEN = 2'd0; end //LB 
 				3'b001: begin `MEM_SIGN = 1'b1; `MEM_LEN = 2'd1; end //LH
 				3'b010:	begin `MEM_SIGN = 1'b1; `MEM_LEN = 2'd2; end //LW
@@ -73,7 +76,7 @@ begin
 		//SB, SH, SW
 		5'b01000: begin 
 			`WB = 1'b1; `MEM_MUX = `aluout_MEM; `MEM_SIGN = 1'b0; `MEM_WEN = 1'b0; `B = 1'b0; `J = 1'b0; `EX_MUX3 = `data1_EX; `EX_MUX2 = `imm_EX; `EX_MUX1 = `data1_EX; `ALU = 4'b0000;
-			case(control_in[7:5])
+			case(control_i[7:5])
 				3'b000: `MEM_LEN = 2'd0; //SB
 				3'b001: `MEM_LEN = 2'd1; //SH
 				3'b010: `MEM_LEN = 2'd2; //SW
@@ -85,15 +88,15 @@ begin
 		5'b0?100: begin 
 			`WB = 1'b0; `MEM_MUX = `aluout_MEM; `MEM_SIGN = 1'b0; `MEM_LEN = 2'b0; `MEM_WEN = 1'b1; `B = 1'b0; `J = 1'b0; `EX_MUX3 = `data1_EX; `EX_MUX1 = `data1_EX;
 			
-			case(control_in[3]) 
+			case(control_i[3]) 
 				1'b0: `EX_MUX2 = `imm_EX;
 				1'b1: `EX_MUX2 = `data2_EX;
 			endcase
 			
-			case(control_in[7:5])
+			case(control_i[7:5])
 				3'b000: begin //ADD, ADDI, SUB
-					if(control_in[3]) //see if it is an add or a subtract
-				 		`ALU = {3'b0,control_in[13]}; //ADD, SUB
+					if(control_i[3]) //see if it is an add or a subtract
+				 		`ALU = {3'b0,control_i[13]}; //ADD, SUB
 				 	else
 				 		`ALU = 4'b0; //ADDI
 				end
@@ -103,7 +106,7 @@ begin
 				3'b011: `ALU = 4'b0101; //SLTU, SLTIU
 				3'b100: `ALU = 4'b0010; //XOR, XORI
 				3'b101: begin //SRA, SRAI, SRL, SRLI
-					if(control_in[13])
+					if(control_i[13])
 						`ALU = 4'b1001; //SRA, SRAI
 					else  
 						`ALU = 4'b1000; //SRL, SRLI
@@ -114,7 +117,7 @@ begin
 			endcase
 		end
 		
-		default: fields_out[15:0] = 16'h41; //nop
+		default: {WB_o, MEM_o, EX_o} = 16'h41; //nop
 	endcase
 		
 end
--- core.v
+++ core.v
@@ -11,6 +11,7 @@ module core(input reset_i,
 	    	output [10:0] data_addr_o,
 	    	output [31:0] data_o); //active-low reset. all write_enable signals are also active-low.
 				  
+
 //IF signals------------------------------------------------------------
 wire [31:0] muxpc_o, plus4_o; //pc-mux output, pc+4 output.
 wire [31:0] addr_cal_o; // address calculation output
@@ -19,20 +20,32 @@ reg [31:0] 	pc_o; //pc out
 wire [10:0] mux_addr_o, mux_load_o, mux_addr_o2;
 wire [31:0] instr_IF;
 wire 		IFID_preg_wen; //IF/ID pipeline register write enable signal.
-reg [63:0] 	IFID_preg; //IF/ID pipeline register.
+//pipeline register
+reg [31:0] IFID_preg_instr;
+reg [31:0] IFID_preg_pc;
 //----------------------------------------------------------------------
 //ID signals
 wire [4:0] 	 rs1_ID, rs2_ID, rd_ID; //register addresses
 wire [31:0]	 data1_ID, data2_ID;
 wire [14:0]  ctrl_unit_i; //control unit input
 wire [29:0]  imm_dec_i; //immediate decoder input
-wire [15:0]  ctrl_unit_o; //control unit output
+//control unit outputs
+wire         ctrl_unit_wb_o;
+wire [5:0]   ctrl_unit_mem_o;
+wire [8:0]   ctrl_unit_ex_o;
 wire [31:0]  imm_dec_o, pc_ID; //immediate decoder output, pc value
 wire 		 mux_ctrl_ID; //control signal for all three muxes
 wire 		 mux1_o_ID; //WB field
 wire [5:0] 	 mux2_o_ID; //MEM field
 wire [8:0] 	 mux3_o_ID; //EX field
-reg  [158:0] IDEX_preg; //ID/EX pipeline register.
+//pipeline register
+reg [31:0] IDEX_preg_imm;
+reg [4:0]  IDEX_preg_rd, IDEX_preg_rs2, IDEX_preg_rs1;
+reg [31:0] IDEX_preg_data2, IDEX_preg_data1;
+reg [31:0] IDEX_preg_pc;
+reg	[8:0]  IDEX_preg_ex;
+reg [5:0]  IDEX_preg_mem;
+reg 	   IDEX_preg_wb;
 
 reg [31:0] register_bank [31:0]; //32x32 register file
 //----------------------------------------------------------------------
@@ -48,7 +61,13 @@ wire [31:0] mux1_o_EX, mux2_o_EX, mux3_o_EX, mux4_o_EX, mux5_o_EX;
 wire [3:0] 	alu_ctrl;
 wire [31:0] aluout_EX;
 wire 		J, B, L; //jump, branch, load
-reg [107:0] EXMEM_preg; //EX/MEM pipeline register
+//pipeline register
+reg [31:0] EXMEM_preg_imm;
+reg [4:0]  EXMEM_preg_rd;
+reg [31:0] EXMEM_preg_data2;
+reg [31:0] EXMEM_preg_aluout;
+reg [5:0]  EXMEM_preg_mem;
+reg        EXMEM_preg_wb;
 //----------------------------------------------------------------------
 //MEM signals-----------------------------------------------------------
 wire 		wb_MEM;
@@ -63,8 +82,12 @@ wire	   wen_MEM_r;
 wire [1:0] ls_length, ls_length_r;
 wire 	   l_sign;
 wire [1:0] mux_ctrl_MEM;
-reg [37:0] MEMWB_preg;
 wire [3:0] wmask0;
+//pipeline register
+reg [4:0]  MEMWB_preg_rd;
+reg [31:0] MEMWB_preg_memout;
+reg        MEMWB_preg_wb;
+reg [37:0] MEMWB_preg;
 //----------------------------------------------------------------------
 //WB signals------------------------------------------------------------
 wire [4:0] 	rd_WB;
@@ -84,13 +107,13 @@ begin
 	begin
 		//reset pc to wherever.
 		pc_o <= 32'h0;
-		IFID_preg[63:0] <= 64'h13; //nop instruction addi x0,x0,0
+		{IFID_preg_pc, IFID_preg_instr} <= 64'h13; //nop instruction addi x0,x0,0
 	end
 	
 	else if(!muxpc_ctrl) //branch taken. next address comes from the address calculation output.
 	begin
-		IFID_preg[31:0] <= 32'h13; //flush the register
-		IFID_preg[63:32] <= addr_cal_o;
+		IFID_preg_instr <= 32'h13; //flush the register
+		IFID_preg_pc    <= addr_cal_o;
 		pc_o <= addr_cal_o + 32'd4;
 	end
 		
@@ -99,11 +122,11 @@ begin
 		if(!IFID_preg_wen) //stall the pipe if necessary
 		begin
 			if(pc_o == 32'h0)
-				IFID_preg[31:0] <= 32'h13;
+				IFID_preg_instr <= 32'h13;
 			else
-				IFID_preg[31:0]	 <= instr_i;
+				IFID_preg_instr <= instr_i;
 				
-			IFID_preg[63:32] <= pc_o - 32'd4 ;
+			IFID_preg_pc <= pc_o - 32'd4 ;
 			pc_o <= pc_o + 32'd4;
 		end
 	end
@@ -112,17 +135,17 @@ end
 
 //ID STAGE---------------------------------------------------------------------------------
 //assign nets
-assign rs1_ID 		= IFID_preg[19:15];
-assign rs2_ID		= IFID_preg[24:20];
-assign rd_ID        = IFID_preg[11:7];
-assign pc_ID 		= IFID_preg[63:32];
-assign imm_dec_i    = IFID_preg[31:2];
-assign ctrl_unit_i  = {IFID_preg[31:25], IFID_preg[14:12], IFID_preg[6:2]};
-assign mux1_o_ID	= mux_ctrl_ID ? 1'b1 : ctrl_unit_o[15];
-assign mux2_o_ID	= mux_ctrl_ID ? 6'b1 : ctrl_unit_o[14:9];
-assign mux3_o_ID	= mux_ctrl_ID ? 9'b0 : ctrl_unit_o[8:0];
+assign rs1_ID 		= IFID_preg_instr[19:15];
+assign rs2_ID		= IFID_preg_instr[24:20];
+assign rd_ID        = IFID_preg_instr[11:7];
+assign pc_ID 		= IFID_preg_pc;
+assign imm_dec_i    = IFID_preg_instr[31:2];
+assign ctrl_unit_i  = {IFID_preg_instr[31:25], IFID_preg_instr[14:12], IFID_preg_instr[6:2]};
+assign mux1_o_ID	= mux_ctrl_ID ? 1'b1 : ctrl_unit_wb_o;
+assign mux2_o_ID	= mux_ctrl_ID ? 6'b1 : ctrl_unit_mem_o;
+assign mux3_o_ID	= mux_ctrl_ID ? 9'b0 : ctrl_unit_ex_o;
 
-control_unit  		  CTRL_UNIT 	(.control_in(ctrl_unit_i), .fields_out(ctrl_unit_o));
+control_unit  		  CTRL_UNIT 	(.control_i(ctrl_unit_i), .WB_o(ctrl_unit_wb_o), .MEM_o(ctrl_unit_mem_o), .EX_o(ctrl_unit_ex_o));
 imm_decoder   		  IMM_DEC   	(.instr_in(imm_dec_i), .imm_out(imm_dec_o));
 
 integer i;
@@ -142,31 +165,31 @@ end
 always @(posedge clk_i or negedge reset_i) 
 begin
 	if(!reset_i)
-		IDEX_preg[158:0] <= {7'b1000001,152'b0};
+		{IDEX_preg_wb, IDEX_preg_mem, IDEX_preg_ex, IDEX_preg_pc, IDEX_preg_data1, IDEX_preg_data2, IDEX_preg_rs1, IDEX_preg_rs2, IDEX_preg_rd, IDEX_preg_imm}  <= {7'b1000001,152'b0};
 		
 	else if(!muxpc_ctrl) //flush the pipe
-		IDEX_preg[158:0] <= {7'b1000001,152'b0};
+		{IDEX_preg_wb, IDEX_preg_mem, IDEX_preg_ex, IDEX_preg_pc, IDEX_preg_data1, IDEX_preg_data2, IDEX_preg_rs1, IDEX_preg_rs2, IDEX_preg_rd, IDEX_preg_imm}  <= {7'b1000001,152'b0};
 	
 	else
 	begin
-		IDEX_preg[31:0]    <= imm_dec_o;
-		IDEX_preg[36:32]   <= rd_ID;
-		IDEX_preg[41:37]   <= rs2_ID;
-		IDEX_preg[46:42]   <= rs1_ID;
-		IDEX_preg[142:111] <= pc_ID;
-		IDEX_preg[158]	   <= mux1_o_ID; 
-		IDEX_preg[157:152] <= mux2_o_ID;
-		IDEX_preg[151:143] <= mux3_o_ID;
+		IDEX_preg_imm <= imm_dec_o;
+		IDEX_preg_rd  <= rd_ID;
+		IDEX_preg_rs2 <= rs2_ID;
+		IDEX_preg_rs1 <= rs1_ID;
+		IDEX_preg_pc  <= pc_ID;
+		IDEX_preg_ex  <= mux3_o_ID;
+		IDEX_preg_mem <= mux2_o_ID;
+		IDEX_preg_wb  <= mux1_o_ID;
 		
 		if(rs1_ID == 5'b0)
-			IDEX_preg[110:79] <= 32'b0;
+			IDEX_preg_data1 <= 32'b0;
 		else
-			IDEX_preg[110:79] <= register_bank[rs1_ID];
+			IDEX_preg_data1 <= register_bank[rs1_ID];
 		
 		if(rs2_ID == 5'b0)
-			IDEX_preg[78:47] <= 32'b0;
+			IDEX_preg_data2 <= 32'b0;
 		else
-			IDEX_preg[78:47] <= register_bank[rs2_ID];
+			IDEX_preg_data2 <= register_bank[rs2_ID];
 	end	
 end
 
@@ -176,16 +199,16 @@ end
 hazard_detection_unit HZRD_DET_UNIT (.rs1(rs1_ID), .rs2(rs2_ID), .idex_rd(rd_EX), .idex_mem(L), .id_mux(mux_ctrl_ID), .ifid_write_en(IFID_preg_wen));
 
 //assign fields
-assign wb_EX    = IDEX_preg[158];
-assign mem_EX   = IDEX_preg[157:152];
-assign ex_EX    = IDEX_preg[151:143];
-assign pc_EX    = IDEX_preg[142:111];
-assign data1_EX = IDEX_preg[110:79];
-assign data2_EX = IDEX_preg[78:47];
-assign rs1_EX   = IDEX_preg[46:42]; 
-assign rs2_EX   = IDEX_preg[41:37];
-assign rd_EX    = IDEX_preg[36:32];
-assign imm_EX   = IDEX_preg[31:0];
+assign wb_EX    = IDEX_preg_wb;
+assign mem_EX   = IDEX_preg_mem;
+assign ex_EX    = IDEX_preg_ex;
+assign pc_EX    = IDEX_preg_pc;
+assign data1_EX = IDEX_preg_data1;
+assign data2_EX = IDEX_preg_data2;
+assign rs1_EX   = IDEX_preg_rs1; 
+assign rs2_EX   = IDEX_preg_rs2;
+assign rd_EX    = IDEX_preg_rd;
+assign imm_EX   = IDEX_preg_imm;
 //assign nets
 assign mux1_ctrl_EX = ex_EX[4];
 assign mux3_ctrl_EX = ex_EX[5];
@@ -226,16 +249,16 @@ assign data_o	   = mux4_o_EX;
 always @(posedge clk_i or negedge reset_i) //clock the outputs to the pipeline register
 begin
 	if(!reset_i)
-		EXMEM_preg[107:0] <= {7'b1000001,101'b0};
+		{EXMEM_preg_wb, EXMEM_preg_mem, EXMEM_preg_aluout, EXMEM_preg_data2, EXMEM_preg_rd, EXMEM_preg_imm} <= {7'b1000001,101'b0};
 
 	else
 	begin
-		EXMEM_preg[31:0] 	<= imm_EX;
-		EXMEM_preg[36:32] 	<= rd_EX;
-		EXMEM_preg[68:37] 	<= mux4_o_EX;
-		EXMEM_preg[100:69] 	<= aluout_EX;
-		EXMEM_preg[106:101] <= mem_EX;
-		EXMEM_preg[107] 	<= wb_EX;
+		EXMEM_preg_imm    <= imm_EX;
+		EXMEM_preg_rd     <= rd_EX;
+		EXMEM_preg_data2  <= mux4_o_EX;
+		EXMEM_preg_aluout <= aluout_EX;
+		EXMEM_preg_mem    <= mem_EX;
+		EXMEM_preg_wb     <= wb_EX;
 	end
 end 
 
@@ -243,14 +266,14 @@ end
 
 //MEM STAGE---------------------------------------------------------------------------------
 //assign fields
-assign wb_MEM 	  = EXMEM_preg[107];
-assign mem_MEM 	  = EXMEM_preg[106:101];
-assign aluout_MEM = EXMEM_preg[100:69];
-assign data2_MEM  = EXMEM_preg[68:37];
-assign rd_MEM 	  = EXMEM_preg[36:32];
-assign imm_MEM 	  = EXMEM_preg[31:0];
+assign wb_MEM 	  = EXMEM_preg_wb;
+assign mem_MEM 	  = EXMEM_preg_mem;
+assign aluout_MEM = EXMEM_preg_aluout;
+assign data2_MEM  = EXMEM_preg_data2;
+assign rd_MEM 	  = EXMEM_preg_rd;
+assign imm_MEM 	  = EXMEM_preg_imm;
 //assign nets
-assign wen_MEM	  = mem_MEM[0];
+assign wen_MEM	    = mem_MEM[0];
 assign ls_length 	= mem_MEM[2:1];
 assign l_sign	    = mem_MEM[3];
 assign mux_ctrl_MEM = mem_MEM[5:4];
@@ -258,51 +281,51 @@ assign mux_ctrl_MEM = mem_MEM[5:4];
 always @(posedge clk_i or negedge reset_i)
 begin
 	if(!reset_i)
-		MEMWB_preg <= {1'b1, 37'b0}; //reset pipeline register
+		{MEMWB_preg_wb, MEMWB_preg_memout, MEMWB_preg_rd} <= {1'b1, 37'b0}; //reset pipeline register
 
 	else
 	begin
 	if(wen_MEM) //not a store instruction
 	begin
 		if(mux_ctrl_MEM == 2'b0)
-			MEMWB_preg[36:5] <= aluout_MEM;
+			MEMWB_preg_memout <= aluout_MEM;
 			
 		else if(mux_ctrl_MEM == 2'b1) //load instruction
 		begin
 			if(ls_length == 2'b0)
 			begin
 				if(l_sign == 2'b1) //signed load, perform sign extension
-					MEMWB_preg[36:5] <= { {24{data_i[7]}}, data_i[7:0] };
+					MEMWB_preg_memout <= { {24{data_i[7]}}, data_i[7:0] };
 				else
-					MEMWB_preg[36:5] <= { 24'b0, data_i[7:0] };
+					MEMWB_preg_memout <= { 24'b0, data_i[7:0] };
 			end
 			else if(ls_length == 2'b1)
 			begin
 				if(l_sign == 2'b1) //signed load, perform sign extension
-					MEMWB_preg[36:5] <= { {16{data_i[15]}}, data_i[15:0] };
+					MEMWB_preg_memout <= { {16{data_i[15]}}, data_i[15:0] };
 				else
-					MEMWB_preg[36:5] <= { 16'b0, data_i[15:0] };			
+					MEMWB_preg_memout <= { 16'b0, data_i[15:0] };			
 			end
 			else
-				MEMWB_preg[36:5] <= { data_i };	
+				MEMWB_preg_memout <= { data_i };	
 		end
 		
 		else
-			MEMWB_preg[36:5] <= imm_MEM;
+			MEMWB_preg_memout <= imm_MEM;
 			
 	end
 
-	MEMWB_preg[37] 	<= wb_MEM;
-	MEMWB_preg[4:0] <= rd_MEM;
+	MEMWB_preg_wb <= wb_MEM;
+	MEMWB_preg_rd <= rd_MEM;
 	end
 end
 //END MEM STAGE-----------------------------------------------------------------------------
 
 //WB STAGE---------------------------------------------------------------------------------
 //assign nets
-assign wb_WB 	 = MEMWB_preg[37];
-assign memout_WB = MEMWB_preg[36:5];
-assign rd_WB 	 = MEMWB_preg[4:0];
+assign wb_WB 	 = MEMWB_preg_wb;
+assign memout_WB = MEMWB_preg_memout;
+assign rd_WB 	 = MEMWB_preg_rd;
 //END WB STAGE-----------------------------------------------------------------------------
 endmodule
 
