899038931658fa343bd189624f021d4fd0b9fdc9
Yavuz Selim <0yavuz0@gmail.com>
2021-02-21 21:25:07 +0300

    memory loading and pipeline registers
    Improved memory loading process. Import opcodes from .data files. Also moved EX/MEM pipeline registers to top module.

--- mtime_registers.v
+++ mtime_registers.v
@@ -1,4 +1,4 @@
-`timescale 1ns/10ps
+`timescale 1ns/1ps
 
 module mtime_registers(input reset_i, csb_i, wen_i, clk_i,
                        input [3:0] addr_i,
@@ -7,14 +7,12 @@ module mtime_registers(input reset_i, csb_i, wen_i, clk_i,
                        
                        output mtip_o,
                        output reg [31:0] data_o);
-                      
-parameter clk_scaler = 100; //divide clk freq. by this value
-parameter cntr_len = 7;
 
 reg [63:0] mtime, mtimecmp;
-reg [cntr_len-1:0] intermediate_counter;
+//reg [cntr_len-1:0] intermediate_counter;
 
 wire [3:0] byte_addr [3:0]; //byte addresses
+wire e_h, l_h, l_l; //greater than, equal and less than for the upper and lower 32 bits (high and low). 
 
 assign byte_addr[0] = addr_i;
 assign byte_addr[1] = addr_i + 4'd1;
@@ -24,81 +22,91 @@ assign byte_addr[3] = addr_i + 4'd3;
 always @(posedge clk_i or negedge reset_i)
 begin
 	if(!reset_i)
-		{mtime, mtimecmp, data_o} <= 160'b0;
+		mtime <= 64'b0;
 		
-	else if(!csb_i)
+	else if(!csb_i & !wen_i)
 	begin
-		if(!wen_i)
+		if(!(wmask_i[3] & !byte_addr[3][3]) & !(wmask_i[2] & !byte_addr[2][3]) & !(wmask_i[1] & !byte_addr[1][3]) & !(wmask_i[0] & !byte_addr[0][3]))
 		begin
-			if(wmask_i[3])
-			begin
-				if(byte_addr[3]) //mtimecmp
-					mtimecmp[8*byte_addr[3][2:0] +: 8] <= data_i[31:24];
-				else
-					mtime[8*byte_addr[3][2:0] +: 8] <= data_i[31:24];
-			end
-			if(wmask_i[2])
-			begin
-				if(byte_addr[3]) //mtimecmp
-					mtimecmp[8*byte_addr[2][2:0] +: 8] <= data_i[23:16];
-				else
-					mtime[8*byte_addr[2][2:0] +: 8] <= data_i[23:16];
-			end
-			if(wmask_i[1])
-			begin
-				if(byte_addr[3]) //mtimecmp
-					mtimecmp[8*byte_addr[1][2:0] +: 8] <= data_i[15:8];
-				else
-					mtime[8*byte_addr[1][2:0] +: 8] <= data_i[15:8];
-			end
-			if(wmask_i[0])
-			begin
-				if(byte_addr[3]) //mtimecmp
-					mtimecmp[8*byte_addr[0][2:0] +: 8] <= data_i[7:0];
-				else
-					mtime[8*byte_addr[0][2:0] +: 8] <= data_i[7:0];
-			end
+			mtime[31:0] <= mtime[31:0] + 32'd1;
+			if(mtime[31:0] == 32'hffff_ffff)
+				mtime[63:32] <= mtime[63:32] + 32'd1;		
 		end
 		
-		else //read
+		else
 		begin
-				if(byte_addr[3]) //mtimecmp
-				begin
-					data_o[31:24] <= mtimecmp[8*byte_addr[3][2:0] +: 8];
-					data_o[23:16] <= mtimecmp[8*byte_addr[2][2:0] +: 8];
-					data_o[15:8] <= mtimecmp[8*byte_addr[1][2:0] +: 8];
-					data_o[7:0] <= mtimecmp[8*byte_addr[0][2:0] +: 8];
-				end
-					
-				else //mtime
-				begin
-					data_o[31:24] <= mtime[8*byte_addr[3][2:0] +: 8];
-					data_o[23:16] <= mtime[8*byte_addr[2][2:0] +: 8];
-					data_o[15:8] <= mtime[8*byte_addr[1][2:0] +: 8];
-					data_o[7:0] <= mtime[8*byte_addr[0][2:0] +: 8];
-				end
-				
-				if(intermediate_counter == clk_scaler-1)
-					mtime <= mtime+64'd1;
-		end
+			if(wmask_i[3] & !byte_addr[3][3])
+				mtime[8*byte_addr[3][2:0] +: 8] <= data_i[31:24];
+
+			if(wmask_i[2] & !byte_addr[2][3])
+				mtime[8*byte_addr[2][2:0] +: 8] <= data_i[23:16];
+			
+			if(wmask_i[1] & !byte_addr[1][3])
+				mtime[8*byte_addr[1][2:0] +: 8] <= data_i[15:8];
+			
+			if(wmask_i[0] & !byte_addr[0][3])
+				mtime[8*byte_addr[0][2:0] +: 8] <= data_i[7:0];		
+		end				
 	end
 	
-	else if(intermediate_counter == clk_scaler-1)
-		mtime <= mtime+64'd1;
+	else
+	begin
+		mtime[31:0] <= mtime[31:0] + 32'd1;
+		if(mtime[31:0] == 32'hffff_ffff)
+			mtime[63:32] <= mtime[63:32] + 32'd1;			
+	end
 end
-
 
 always @(posedge clk_i or negedge reset_i)
 begin
 	if(!reset_i)
-		intermediate_counter <= 0;
-	else if(intermediate_counter == clk_scaler-1)
-		intermediate_counter <= 0;
-	else
-		intermediate_counter <= intermediate_counter + 1;
+		mtimecmp <= 64'b0;
+		
+	else if(!csb_i & !wen_i)
+	begin
+		if(wmask_i[3] & byte_addr[3][3])
+			mtimecmp[8*byte_addr[3][2:0] +: 8] <= data_i[31:24];
+
+		if(wmask_i[2] & byte_addr[2][3])
+			mtimecmp[8*byte_addr[2][2:0] +: 8] <= data_i[23:16];
+			
+		if(wmask_i[1] & byte_addr[1][3])
+			mtimecmp[8*byte_addr[1][2:0] +: 8] <= data_i[15:8];
+			
+		if(wmask_i[0] & byte_addr[0][3])
+			mtimecmp[8*byte_addr[0][2:0] +: 8] <= data_i[7:0];		
+	end
 end
 
-assign mtip_o = mtime >= mtimecmp ? 1'b1 : 1'b0;
+always @(*)
+begin
+	if(byte_addr[3][3]) //mtimecmp
+		data_o[31:24] <= mtimecmp[8*byte_addr[3][2:0] +: 8];
+	else
+		data_o[31:24] <= mtime[8*byte_addr[3][2:0] +: 8];
+		
+	if(byte_addr[2][3]) //mtimecmp
+		data_o[23:16] <= mtimecmp[8*byte_addr[2][2:0] +: 8];
+	else
+		data_o[23:16] <= mtime[8*byte_addr[2][2:0] +: 8];
+			
+	if(byte_addr[1][3]) //mtimecmp
+		data_o[15:8] <= mtimecmp[8*byte_addr[1][2:0] +: 8];
+	else
+		data_o[15:8] <= mtime[8*byte_addr[1][2:0] +: 8];
+		
+	if(byte_addr[0][3]) //mtimecmp
+		data_o[7:0] <= mtimecmp[8*byte_addr[0][2:0] +: 8];
+	else
+		data_o[7:0] <= mtime[8*byte_addr[0][2:0] +: 8];
+end
+
+assign e_h = mtime[63:32] == mtimecmp[63:32];
+assign l_h = mtime[63:32] < mtimecmp[63:32];
+assign l_l = mtime[31:0] < mtimecmp[31:0];
+
+assign mtip_o = !(l_h | (e_h & l_l)); 
+//assign mtip_o = mtime >= mtimecmp ? 1'b1 : 1'b0;
 
 endmodule
 
--- top_module.v
+++ top_module.v
@@ -1,53 +1,90 @@
 `include "/home/ytozlu/projects/BASAK/projectDir/yavuz_digital/riscv/top_module/core.v"
 `include "/home/ytozlu/projects/BASAK/projectDir/yavuz_digital/riscv/top_module/mtime_registers.v"
-
+`include "/home/ytozlu/projects/BASAK/projectDir/yavuz_digital/riscv/top_module/uart.v"
 `timescale 1ns/1ps
 
 module top_module(input clk_i,
                   input reset_i,
-                  input wen_i, meip_i,
-                  input [31:0] addr_i,
-                  input [31:0] instr_i,
+                  input rx_i,
+                  output tx_o,
                   output irq_ack_o);
 				  
-wire [3:0] wmask0, wmask1;
+wire [3:0] wmask0;
 wire wen0, mtip_o;
 wire [31:0] instr_addr_o, data_addr_o;
-wire [31:0] mem_data_o, mem_instr_o, data_o, mtime_reg_data_o, core_data_i;
-wire [31:0] mux_addr_o;
+wire [31:0] mem_data_o, mem_instr_o, core_data_o, mtime_data_o, core_data_i;
 wire sram_csb, mtime_csb;
 
-reg csb_reg;
+reg mtime_csb_reg;
+reg [31:0] data_addr_o_reg, core_data_o_reg;
+reg [3:0] wmask0_reg;
+reg wen0_reg;
+
+wire uart_csb, uart_rx_irq;
+wire [7:0] uart_data_o;
 
 assign sram_csb = data_addr_o[11] | !reset_i;
-assign mtime_csb = !data_addr_o[11] | !reset_i;
-assign mux_addr_o = wen_i ? instr_addr_o : addr_i;
+assign mtime_csb = !data_addr_o[11] | data_addr_o[4] | !reset_i;
+assign uart_csb = !data_addr_o_reg[11] | !data_addr_o_reg[4] | !reset_i;
+
+assign core_data_i = !mtime_csb_reg ? mtime_data_o 
+                   : !uart_csb ? uart_data_o
+                   : mem_data_o;
 
 always @(posedge clk_i or negedge reset_i)
 begin
 	if(!reset_i)
-		csb_reg <= 1'b0;
+		mtime_csb_reg <= 1'b1;
 	else
-		csb_reg <= sram_csb;
+		mtime_csb_reg <= mtime_csb;
 end
-assign core_data_i = csb_reg ? mtime_reg_data_o : mem_data_o;
+
+//register the inputs to peripherals
+always @(posedge clk_i or negedge reset_i)
+begin
+	if(!reset_i)
+		{data_addr_o_reg,core_data_o_reg,wmask0_reg,wen0_reg} <= 69'b0;
+	else
+	begin
+		data_addr_o_reg <= data_addr_o;
+		core_data_o_reg <= core_data_o;
+		wmask0_reg <= wmask0;
+		wen0_reg <= wen0;
+	end
+end
+
 core core0(.clk_i(clk_i), 
            .reset_i(reset_i), 
-           .meip_i(meip_i), 
+           .meip_i(uart_rx_irq), 
            .mtip_i(mtip_o), 
            .instr_i(mem_instr_o), 
            .data_i(core_data_i), 
-           .data_o(data_o), 
+           .data_o(core_data_o), 
            .wmask0_o(wmask0), 
            .wen0_o(wen0), 
            .instr_addr_o(instr_addr_o), 
            .data_addr_o(data_addr_o), 
            .irq_ack_o(irq_ack_o));
 
-memory_2rw memory(.clk0(clk_i), .csb0(sram_csb), .web0(wen0), .wmask0(wmask0), .addr0(data_addr_o[10:2]), .din0(data_o), .dout0(mem_data_o), //port 0 is for data memory
-                  .clk1(clk_i), .csb1(1'b0), .web1(wen_i), .wmask1(4'b1111), .addr1(mux_addr_o[10:2]), .din1(instr_i), .dout1(mem_instr_o)); //port 1 is for instruction memory
+memory_2rw memory(.clk0(clk_i), .csb0(sram_csb), .web0(wen0), .wmask0(wmask0), .addr0(data_addr_o[10:2]), .din0(core_data_o), .dout0(mem_data_o), //port 0 is for data memory
+                  .clk1(clk_i), .csb1(1'b0), .web1(1'b1), .wmask1(4'b1111), .addr1(instr_addr_o[10:2]), .din1(32'b0), .dout1(mem_instr_o)); //port 1 is for instruction memory
                                      
-mtime_registers mtime0(.reset_i(reset_i), .csb_i(mtime_csb), .wen_i(wen0), .clk_i(clk_i),
-                .addr_i(data_addr_o[3:0]), .data_i(data_o), .wmask_i(wmask0), .mtip_o(mtip_o), .data_o(mtime_reg_data_o));                                     
+mtime_registers mtime0(.reset_i(reset_i), 
+                       .csb_i(mtime_csb_reg), 
+                       .wen_i(wen0_reg), 
+                       .clk_i(clk_i),
+                       .addr_i(data_addr_o_reg[3:0]), 
+                       .data_i(core_data_o_reg), 
+                       .wmask_i(wmask0_reg), 
+                       .mtip_o(mtip_o), 
+                       .data_o(mtime_data_o));  
+                       
+uart uart0 (.clk_i(clk_i), .reset_i(reset_i), .rx_i(rx_i),
+            .csb_i(uart_csb), .wen_i(wen0_reg),
+            .data_i(core_data_o_reg[7:0]),
+            .addr_i(data_addr_o_reg[1:0]),
+            .tx_o(tx_o), .receive_irq(uart_rx_irq),
+            .data_o(uart_data_o));                                   
 
 endmodule
+
