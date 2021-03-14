c9918348e4710551261afff4a866c5a367ab1221
yavuz650 <tozlu16@itu.edu.tr>
2021-03-15 01:45:02 +0300

    improved coding
    removed redundant volatile specifiers, and did minor improvements.

--- test/itucore.h
+++ test/itucore.h
@@ -1,48 +1,44 @@
 #define MTIME_ADDR 0x00000800
 #define MTIMECMP_ADDR 0x00000808
 
-#define ENABLE_GLOBAL_IRQ() __asm__("csrrsi zero,0x300,0x8\n"); //enables interrupts globally by setting the mie bit in mstatus register.
-#define DISABLE_GLOBAL_IRQ() __asm__("csrrci zero,0x300,0x8\n"); //disables interrupts globally by clearing the mie bit in mstatus register.
+#define ENABLE_GLOBAL_IRQ() __asm__("csrsi mstatus,0x8\n"); //enables interrupts globally by setting the mie bit in mstatus register.
+#define DISABLE_GLOBAL_IRQ() __asm__("csrci mstatus,0x8\n"); //disables interrupts globally by clearing the mie bit in mstatus register.
 
 //sets mtvec's value to the beginning of the vector table.
 static inline void SET_MTVEC_VECTOR_MODE()
 {
-    __asm__ volatile(
-            "addi sp,sp,-4\n"
-            "sw t0,0(sp)\n"
-            "la t0, exc\n" 
-            "slli t0,t0,2\n" 
-            "ori t0,t0,1\n" 
-            "csrrw zero,0x305,t0\n"
-            "lw t0,0(sp)\n"
-            "addi sp,sp,4\n");
+    int base_addr;
+    __asm__ volatile ("la %[base_addr],exc" :[base_addr] "=r" (base_addr): );
+    base_addr = base_addr << 2;
+    base_addr |= 0x1;
+    __asm__ volatile ("csrw mtvec,%[base_addr]" :: [base_addr] "r" (base_addr));
 }
 
 //enables machine level timer interrupts by setting the mtie bit in mie register.
 static inline void ENABLE_MTI()
 {
-    volatile int mask = 0x80;
-    __asm__ volatile ("csrrs zero,0x304,%[mask]" :: [mask] "r" (mask));
+    int mask = 0x80;
+    __asm__ volatile ("csrs mie,%[mask]" :: [mask] "r" (mask));
 }
 
 //disables machine level timer interrupts by clearing the mtie bit in mie register.
 static inline void DISABLE_MTI()
 {
-    volatile int mask = 0x80;
-    __asm__ volatile ("csrrc zero,0x304,%[mask]" :: [mask] "r" (mask));
+    int mask = 0x80;
+    __asm__ volatile ("csrc mie,%[mask]" :: [mask] "r" (mask));
 }
 
 //enables machine level external interrupts by setting the meie bit in mie register.
 static inline void ENABLE_MEI()
 {
-    volatile int mask = 0x800;
-    __asm__ volatile ("csrrs zero,0x304,%[mask]" :: [mask] "r" (mask));
+    int mask = 0x800;
+    __asm__ volatile ("csrs mie,%[mask]" :: [mask] "r" (mask));
 }
 //disables machine level external interrupts by clearing the meie bit in mie register.
 static inline void DISABLE_MEI()
 {
-    volatile int mask = 0x800;
-    __asm__ volatile ("csrrc zero,0x304,%[mask]" :: [mask] "r" (mask));
+    int mask = 0x800;
+    __asm__ volatile ("csrc mie,%[mask]" :: [mask] "r" (mask));
 }
 
 void mei_handler() __attribute__ (( interrupt ("machine")));
