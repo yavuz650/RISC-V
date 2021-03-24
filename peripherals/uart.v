9314384b2555f64ceb7bb9a3bbd55803aaf1bf8f
yavuz650 <tozlu16@itu.edu.tr>
2021-03-24 03:49:00 +0300

    Added a uart module.

---
+++ peripherals/uart.v
@@ -0,0 +1,329 @@
+`timescale 1ns/1ps
+
+module uart(input clk_i, reset_i, rx_i,
+            input csb_i, wen_i,
+            input [7:0] data_i,
+            input [3:0] wmask_i,
+            output tx_o, receive_irq,
+            output [31:0] data_o);
+            
+wire [2:0] uart_status;
+wire [7:0] rx_byte;
+wire transmit;
+wire tx_irq;
+assign transmit = !wen_i & !csb_i & wmask_i[0];
+
+assign data_o = {8'b0,5'b0,uart_status,rx_byte,8'b0};
+assign uart_status[0] = 1'b0;
+assign uart_status[2] = 1'b0;
+UART_TX uart_tx0
+  (
+      .i_Rst_L(reset_i),
+      .i_Clock(clk_i),
+      .i_TX_DV(transmit),
+      .i_TX_Byte(data_i), 
+      .o_TX_Active(uart_status[1]),
+      .o_TX_Serial(tx_o),
+      .o_TX_Done(tx_irq));
+      
+UART_RX uart_rx0
+        (  .i_Rst_L(reset_i),
+           .i_Clock(clk_i),
+           .i_RX_Serial(rx_i),
+           .o_RX_DV(receive_irq),
+           .o_RX_Byte(rx_byte));
+
+endmodule
+
+// UART modules from nandland @ github.com/nandland
+// Author: Russell Merrick
+
+// This file contains the UART Transmitter.  This transmitter is able
+// to transmit 8 bits of serial data, one start bit, one stop bit,
+// and no parity bit.  When transmit is complete o_Tx_done will be
+// driven high for one clock cycle.
+//
+// Set Parameter CLKS_PER_BIT as follows:
+// CLKS_PER_BIT = (Frequency of i_Clock)/(Frequency of UART)
+// Example: 25 MHz Clock, 115200 baud UART
+// (25000000)/(115200) = 217
+ 
+module UART_TX 
+  #(parameter CLKS_PER_BIT = 1250)
+  (
+   input       i_Rst_L,
+   input       i_Clock,
+   input       i_TX_DV,
+   input [7:0] i_TX_Byte, 
+   output reg  o_TX_Active,
+   output reg  o_TX_Serial,
+   output reg  o_TX_Done
+   );
+ 
+  localparam IDLE         = 3'b000;
+  localparam TX_START_BIT = 3'b001;
+  localparam TX_DATA_BITS = 3'b010;
+  localparam TX_STOP_BIT  = 3'b011;
+  localparam CLEANUP      = 3'b100;
+  
+  reg [2:0] r_SM_Main;
+  reg [$clog2(CLKS_PER_BIT):0] r_Clock_Count;
+  reg [2:0] r_Bit_Index;
+  reg [7:0] r_TX_Data;
+
+
+  // Purpose: Control TX state machine
+  always @(posedge i_Clock or negedge i_Rst_L)
+  begin
+    if (~i_Rst_L)
+    begin
+      r_SM_Main <= 3'b000;
+      o_TX_Done <= 1'b0;
+      o_TX_Active <= 1'b0;
+    end
+    else
+    begin
+      case (r_SM_Main)
+      IDLE :
+        begin
+          o_TX_Serial   <= 1'b1;         // Drive Line High for Idle
+          o_TX_Done     <= 1'b0;
+          r_Clock_Count <= 0;
+          r_Bit_Index   <= 0;
+          
+          if (i_TX_DV == 1'b1)
+          begin
+            o_TX_Active <= 1'b1;
+            r_TX_Data   <= i_TX_Byte;
+            r_SM_Main   <= TX_START_BIT;
+          end
+          else
+            r_SM_Main <= IDLE;
+        end // case: IDLE
+      
+      
+      // Send out Start Bit. Start bit = 0
+      TX_START_BIT :
+        begin
+          o_TX_Serial <= 1'b0;
+          
+          // Wait CLKS_PER_BIT-1 clock cycles for start bit to finish
+          if (r_Clock_Count < CLKS_PER_BIT-1)
+          begin
+            r_Clock_Count <= r_Clock_Count + 1;
+            r_SM_Main     <= TX_START_BIT;
+          end
+          else
+          begin
+            r_Clock_Count <= 0;
+            r_SM_Main     <= TX_DATA_BITS;
+          end
+        end // case: TX_START_BIT
+      
+      
+      // Wait CLKS_PER_BIT-1 clock cycles for data bits to finish         
+      TX_DATA_BITS :
+        begin
+          o_TX_Serial <= r_TX_Data[r_Bit_Index];
+          
+          if (r_Clock_Count < CLKS_PER_BIT-1)
+          begin
+            r_Clock_Count <= r_Clock_Count + 1;
+            r_SM_Main     <= TX_DATA_BITS;
+          end
+          else
+          begin
+            r_Clock_Count <= 0;
+            
+            // Check if we have sent out all bits
+            if (r_Bit_Index < 7)
+            begin
+              r_Bit_Index <= r_Bit_Index + 1;
+              r_SM_Main   <= TX_DATA_BITS;
+            end
+            else
+            begin
+              r_Bit_Index <= 0;
+              r_SM_Main   <= TX_STOP_BIT;
+            end
+          end 
+        end // case: TX_DATA_BITS
+      
+      
+      // Send out Stop bit.  Stop bit = 1
+      TX_STOP_BIT :
+        begin
+          o_TX_Serial <= 1'b1;
+          
+          // Wait CLKS_PER_BIT-1 clock cycles for Stop bit to finish
+          if (r_Clock_Count < CLKS_PER_BIT-1)
+          begin
+            r_Clock_Count <= r_Clock_Count + 1;
+            r_SM_Main     <= TX_STOP_BIT;
+          end
+          else
+          begin
+            o_TX_Done     <= 1'b1;
+            r_Clock_Count <= 0;
+            r_SM_Main     <= CLEANUP;
+            o_TX_Active   <= 1'b0;
+          end 
+        end // case: TX_STOP_BIT
+      
+      
+      // Stay here 1 clock
+      CLEANUP :
+        begin
+          r_SM_Main <= IDLE;
+        end
+      
+      
+      default :
+        r_SM_Main <= IDLE;
+      
+    endcase
+    end // else: !if(~i_Rst_L)
+  end // always @ (posedge i_Clock or negedge i_Rst_L)
+
+  
+endmodule
+
+//////////////////////////////////////////////////////////////////////
+// Description: This file contains the UART Receiver.  This receiver is 
+//              able to receive 8 bits of serial data, one start bit, one 
+//              stop bit, and no parity bit.  When receive is complete 
+//              o_RX_DV will be driven high for one clock cycle.
+// 
+// Parameters:  Set Parameter CLKS_PER_BIT as follows:
+//              CLKS_PER_BIT = (Frequency of i_Clock)/(Frequency of UART)
+//              Example: 25 MHz Clock, 115200 baud UART
+//              (25000000)/(115200) = 217
+//////////////////////////////////////////////////////////////////////////////
+
+module UART_RX
+  #(parameter CLKS_PER_BIT = 1250)
+  (
+   input            i_Rst_L,
+   input            i_Clock,
+   input            i_RX_Serial,
+   output reg       o_RX_DV,
+   output reg [7:0] o_RX_Byte
+   );
+   
+  localparam IDLE         = 3'b000;
+  localparam RX_START_BIT = 3'b001;
+  localparam RX_DATA_BITS = 3'b010;
+  localparam RX_STOP_BIT  = 3'b011;
+  localparam CLEANUP      = 3'b100;
+  
+  reg [$clog2(CLKS_PER_BIT)-1:0] r_Clock_Count;
+  reg [2:0] r_Bit_Index; //8 bits total
+  reg [2:0] r_SM_Main;
+  
+  
+  // Purpose: Control RX state machine
+  always @(posedge i_Clock or negedge i_Rst_L)
+  begin
+    if (~i_Rst_L)
+    begin
+      r_SM_Main <= 3'b000;
+      o_RX_DV   <= 1'b0;
+    end
+    else
+    begin
+      case (r_SM_Main)
+      IDLE :
+        begin
+          o_RX_DV       <= 1'b0;
+          r_Clock_Count <= 0;
+          r_Bit_Index   <= 0;
+          
+          if (i_RX_Serial == 1'b0)          // Start bit detected
+            r_SM_Main <= RX_START_BIT;
+          else
+            r_SM_Main <= IDLE;
+        end
+      
+      // Check middle of start bit to make sure it's still low
+      RX_START_BIT :
+        begin
+          if (r_Clock_Count == (CLKS_PER_BIT-1)/2)
+          begin
+            if (i_RX_Serial == 1'b0)
+            begin
+              r_Clock_Count <= 0;  // reset counter, found the middle
+              r_SM_Main     <= RX_DATA_BITS;
+            end
+            else
+              r_SM_Main <= IDLE;
+          end
+          else
+          begin
+            r_Clock_Count <= r_Clock_Count + 1;
+            r_SM_Main     <= RX_START_BIT;
+          end
+        end // case: RX_START_BIT
+      
+      
+      // Wait CLKS_PER_BIT-1 clock cycles to sample serial data
+      RX_DATA_BITS :
+        begin
+          if (r_Clock_Count < CLKS_PER_BIT-1)
+          begin
+            r_Clock_Count <= r_Clock_Count + 1;
+            r_SM_Main     <= RX_DATA_BITS;
+          end
+          else
+          begin
+            r_Clock_Count          <= 0;
+            o_RX_Byte[r_Bit_Index] <= i_RX_Serial;
+            
+            // Check if we have received all bits
+            if (r_Bit_Index < 7)
+            begin
+              r_Bit_Index <= r_Bit_Index + 1;
+              r_SM_Main   <= RX_DATA_BITS;
+            end
+            else
+            begin
+              r_Bit_Index <= 0;
+              r_SM_Main   <= RX_STOP_BIT;
+            end
+          end
+        end // case: RX_DATA_BITS
+      
+      
+      // Receive Stop bit.  Stop bit = 1
+      RX_STOP_BIT :
+        begin
+          // Wait CLKS_PER_BIT-1 clock cycles for Stop bit to finish
+          if (r_Clock_Count < CLKS_PER_BIT-1)
+          begin
+            r_Clock_Count <= r_Clock_Count + 1;
+            r_SM_Main     <= RX_STOP_BIT;
+          end
+          else
+          begin
+            o_RX_DV       <= 1'b1;
+            r_Clock_Count <= 0;
+            r_SM_Main     <= CLEANUP;
+          end
+        end // case: RX_STOP_BIT
+      
+      
+      // Stay here 1 clock
+      CLEANUP :
+        begin
+          r_SM_Main <= IDLE;
+          o_RX_DV   <= 1'b0;
+        end
+      
+      
+      default :
+        r_SM_Main <= IDLE;
+      
+    endcase
+    end // else: !if(~i_Rst_L)
+  end // always @ (posedge i_Clock or negedge i_Rst_L)
+  
+endmodule // UART_RX
