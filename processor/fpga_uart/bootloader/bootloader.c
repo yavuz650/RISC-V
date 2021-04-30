844ed383cc23489af2ae7da4c65e250b1711cade
Yavuz Selim Tozlu <0yavuz0@gmail.com>
2021-04-30 17:20:49 +0300

    Transition to fast interrupts.
    bootloader and aes_main now use a fast interrupt instead of the external interrupt.

--- processor/fpga_uart/bootloader/bootloader.c
+++ processor/fpga_uart/bootloader/bootloader.c
@@ -1,6 +1,6 @@
 #include "../../../lib/uart.h"
 #include "../../../lib/irq.h"
-
+#include <stdlib.h>
 int ram_index;
 uart uart0;
 
@@ -8,7 +8,6 @@ int main()
 {
     ram_index=0;
     SET_MTVEC_VECTOR_MODE();
-    
     uart_init(&uart0,0x00008010);
     uart_transmit_string(&uart0,"Waiting for opcodes...\n",23);
 
@@ -18,11 +17,6 @@ int main()
     while(1);
 }
 
-void mti_handler() {}
-void mei_handler() {}
-void msi_handler() {}
-void exc_handler() {}
-
 void fast_irq0_handler()
 {
     char *rx_ptr = (char*)(uart0.base_addr)+UART_RX_ADDR_OFFSET;
@@ -31,4 +25,5 @@ void fast_irq0_handler()
     ram_index++;
 }
 
-void fast_irq1_handler() {}
+void mei_handler() {}
+
