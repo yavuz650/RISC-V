bf7da8182256bd9c300db63ef36f8949293d295b
Yavuz Selim Tozlu <0yavuz0@gmail.com>
2021-05-08 17:10:15 +0300

    Added access-fault exceptions.
    Processor now checks for illegal access violations, i.e. stores to read-only sections. Also improved the readability of the code.

--- processor/barebones/barebones_top.v
+++ processor/barebones/barebones_top.v
@@ -5,86 +5,162 @@ module barebones_top(input clk_i,
                      input meip_i,
                      input [15:0] fast_irq_i,
                      output irq_ack_o);
-				   
-wire [3:0] data_wmask;
-wire data_wen, mtip_o;
-wire [31:0] instr_addr_o, data_addr_o;
-wire [31:0] mem_data_o, mem_instr_o, core_data_o, mtime_data_o, core_data_i;
-wire sram_csb, mtime_csb;
 
+//core data interface
+wire [31:0] core_data_addr;
+wire [31:0] core_data_out;
+wire [31:0] core_data_in;
+wire [3:0]  core_data_wmask;
+wire        core_data_wen;
+wire        core_data_req;
+wire        core_data_err;
+//core instruction interface
+wire [31:0] core_instr_addr;
+wire [31:0] core_instr_in;
+wire        core_instr_access_fault;
+//registered interface signals
+reg [31:0] core_data_addr_reg;
+reg [31:0] core_data_out_reg;
+reg [3:0]  core_data_wmask_reg;
+reg        core_data_wen_reg;
+reg        core_data_req_reg;
+reg [31:0] core_instr_addr_reg;
+
+//memory data port
+wire [31:0] mem_data_in;
+wire [31:0] mem_data_out;
+wire [31:0] mem_data_addr;
+wire        mem_data_wen;
+wire        mem_data_csb;
+wire [3:0]  mem_data_wmask;
+//memory instruction port
+wire [31:0] mem_instr_out;
+wire [31:0] mem_instr_addr;
+
+//timer register signals
+wire [31:0] mtime_data_out;
+
+//chip-select signals, all active-low
+wire ram_csb; //Read-Write region of memory
+wire rom_csb; //Read-only region of memory
+wire mem_csb; //Combination of RAM and ROM regions
+wire mtime_csb; //Timer registers
+reg ram_csb_reg;
+reg rom_csb_reg;
+reg mem_csb_reg;
 reg mtime_csb_reg;
-reg [31:0] data_addr_o_reg, core_data_o_reg;
-reg [3:0] data_wmask_reg;
-reg data_wen_reg;
 
+//timer interrupt signal
+wire mtip;
 
-assign sram_csb = !(data_addr_o[31:13] == 19'b0) | !reset_i;
-assign mtime_csb = !(data_addr_o[31:4] == 27'h200) | !reset_i;
+//internal nets
+wire masked_wen;
+reg masked_wen_reg;
 
-assign core_data_i = !mtime_csb_reg ? mtime_data_o : mem_data_o;
-               
+//0x0000_0000 to 0x0000_1DFF is ROM
+assign rom_csb = !((core_data_addr[31:13] == 19'b0) & (core_data_addr[12:9] != 4'hf)) | !reset_i;
+//0x0000_1E00 to 0x0000_1FFF is RAM
+assign ram_csb = !((core_data_addr[31:13] == 19'b0) & (core_data_addr[12:9] == 4'hf)) | !reset_i;
+assign mem_csb = ram_csb & rom_csb;
+//0x0000_2000 to 0x0000_200F is mtime and mtimecmp, in order.
+assign mtime_csb = !(core_data_addr[31:4] == 27'h200) | !reset_i;
+
+//if the instruction address falls outside of ROM, then the instruction access failed.
+assign core_instr_access_fault = !((core_instr_addr_reg[31:13] == 19'b0) & (core_instr_addr_reg[12:9] != 4'hf));
+//attempting to write to ROM will raise an error.
+assign core_data_err = core_data_req_reg & ~rom_csb_reg & ~core_data_wen_reg;
+//writes are only allowed in RAM region
+assign masked_wen = ram_csb | core_data_wen;
+
+assign core_data_in = !mtime_csb_reg ? mtime_data_out : mem_data_out;
+assign core_instr_in = mem_instr_out;
+
+assign mem_data_in = core_data_out;
+assign mem_data_addr = core_data_addr;
+assign mem_data_wen = masked_wen;
+assign mem_data_csb = mem_csb;
+assign mem_data_wmask = core_data_wmask;
+assign mem_instr_addr = core_instr_addr;
+
+//register the interface signals
 always @(posedge clk_i or negedge reset_i)
 begin
-	if(!reset_i)
-		mtime_csb_reg <= 1'b1;
-	else
-		mtime_csb_reg <= mtime_csb;
-end
+    if(!reset_i)
+    begin
+        core_data_addr_reg <= 32'b0;
+        core_data_out_reg <= 32'b0;
+        core_data_wmask_reg <= 4'b0;
+        core_data_wen_reg <= 1'b1;
+        core_data_req_reg <= 1'b0;
+        core_instr_addr_reg <= 32'b0;
+        rom_csb_reg <= 1'b1;
+        ram_csb_reg <= 1'b1;
+        mem_csb_reg <= 1'b1;
+        mtime_csb_reg <= 1'b1;
+        masked_wen_reg <= 1'b1;
+    end
 
-//register the inputs to peripherals
-always @(posedge clk_i or negedge reset_i)
-begin
-	if(!reset_i)
-		{data_addr_o_reg,core_data_o_reg,data_wmask_reg,data_wen_reg} <= 69'b0;
 	else
 	begin
-		data_addr_o_reg <= data_addr_o;
-		core_data_o_reg <= core_data_o;
-		data_wmask_reg <= data_wmask;
-		data_wen_reg <= data_wen;
+        core_data_addr_reg <= core_data_addr;
+        core_data_out_reg <= core_data_out;
+        core_data_wmask_reg <= core_data_wmask;
+        core_data_wen_reg <= core_data_wen;
+        core_data_req_reg <= core_data_req;
+        core_instr_addr_reg <= core_instr_addr;
+        rom_csb_reg <= rom_csb;
+        ram_csb_reg <= ram_csb;
+        mem_csb_reg <= mem_csb;
+        mtime_csb_reg <= mtime_csb;
+        masked_wen_reg <= masked_wen;
 	end
 end
 
 core core0(.clk_i(clk_i),
            .hreset_i(reset_i),
            .sreset_i(1'b1),
+
+           .data_addr_o(core_data_addr),
+           .data_i(core_data_in),
+           .data_o(core_data_out),
+           .data_wmask_o(core_data_wmask),
+           .data_wen_o(core_data_wen),
+           .data_req_o(core_data_req),
+           .data_err_i(core_data_err),
+
+           .instr_addr_o(core_instr_addr),
+           .instr_i(core_instr_in),
+           .instr_access_fault_i(core_instr_access_fault),
+
            .meip_i(meip_i),
-           .mtip_i(mtip_o),
+           .mtip_i(mtip),
            .msip_i(1'b0),
            .fast_irq_i(fast_irq_i),
-           .instr_i(mem_instr_o),
-           .data_i(core_data_i),
-           .data_o(core_data_o),
-           .data_wmask_o(data_wmask),
-           .data_wen_o(data_wen),
-           .instr_addr_o(instr_addr_o),
-           .data_addr_o(data_addr_o),
            .irq_ack_o(irq_ack_o));
 
-memory_2rw #(.ADDR_WIDTH(11)) memory(.clk0(clk_i), 
-                  .csb0(sram_csb), 
-                  .web0(data_wen), 
-                  .wmask0(data_wmask), 
-                  .addr0(data_addr_o[12:2]), 
-                  .din0(core_data_o), 
-                  .dout0(mem_data_o), //port 0 is for data memory
-                  .clk1(clk_i), 
-                  .csb1(1'b0), 
-                  .web1(1'b1), 
-                  .wmask1(4'b1111), 
-                  .addr1(instr_addr_o[12:2]), 
-                  .din1(32'b0), 
-                  .dout1(mem_instr_o)); //port 1 is for instruction memory
-                                     
+memory_2rw #(.ADDR_WIDTH(11)) memory(.clk0(clk_i),
+                  .csb0(mem_data_csb),
+                  .web0(mem_data_wen),
+                  .wmask0(mem_data_wmask),
+                  .addr0(mem_data_addr[12:2]),
+                  .din0(mem_data_in),
+                  .dout0(mem_data_out), //port 0 is for data memory
+                  .clk1(clk_i),
+                  .csb1(1'b0),
+                  .web1(1'b1),
+                  .wmask1(4'b1111),
+                  .addr1(mem_instr_addr[12:2]),
+                  .din1(32'b0),
+                  .dout1(mem_instr_out)); //port 1 is for instruction memory
+
 mtime_registers mtime0(.reset_i(reset_i),
                        .csb_i(mtime_csb_reg),
-                       .wen_i(data_wen_reg),
+                       .wen_i(core_data_wen_reg),
                        .clk_i(clk_i),
-                       .addr_i(data_addr_o_reg[3:0]),
-                       .data_i(core_data_o_reg),
-                       .wmask_i(data_wmask_reg),
-                       .mtip_o(mtip_o),
-                       .data_o(mtime_data_o));
+                       .addr_i(core_data_addr_reg[3:0]),
+                       .data_i(core_data_out_reg),
+                       .wmask_i(core_data_wmask_reg),
+                       .mtip_o(mtip),
+                       .data_o(mtime_data_out));
 
 endmodule
-
--- processor/barebones/barebones_top_tb.v
+++ processor/barebones/barebones_top_tb.v
@@ -9,11 +9,13 @@ reg [15:0] fast_irq_i;
 
 barebones_top uut(.reset_i(reset_i), .clk_i(clk_i), .meip_i(meip_i), .fast_irq_i(fast_irq_i), .irq_ack_o(irq_ack_o));
 
+//100 MHz clock
 always begin
 clk_i = 1'b0; #5; clk_i = 1'b1; #5;
 end
 
 initial begin
+//uncomment the program you want to simulate
 //$readmemh("../../test/memory_contents/bubble_sort_irq.data",uut.memory.mem);
 //$readmemh("../../test/memory_contents/bubble_sort.data",uut.memory.mem);
 $readmemh("../../test/memory_contents/aes_test.data",uut.memory.mem);
@@ -22,26 +24,26 @@ reset_i = 1'b0; fast_irq_i = 16'b0; meip_i = 1'b0;
 #200;
 reset_i = 1'b1;
 
-//interrupt signals, arbitrarily generated.
+//interrupt signals, arbitrarily generated. uncomment if you need to.
 /*
-#2100; fast_irq_i=1'b1; 
-#400;  fast_irq_i=1'b1;
-#400;  fast_irq_i=1'b1; 
-#400;  fast_irq_i=1'b1;
-#850;  fast_irq_i=1'b1;
-#316;  fast_irq_i=1'b1;
-#763;  fast_irq_i=1'b1;
-#152;  fast_irq_i=1'b1;
-#761;  fast_irq_i=1'b1;
-#252;  fast_irq_i=1'b1;*/
-fast_irq_i = 16'h1; #10; fast_irq_i = 16'h0; 
+#2100; meip_i=1'b1; 
+#400;  meip_i=1'b1;
+#400;  meip_i=1'b1; 
+#400;  meip_i=1'b1;
+#850;  meip_i=1'b1;
+#316;  meip_i=1'b1;
+#763;  meip_i=1'b1;
+#152;  meip_i=1'b1;
+#761;  meip_i=1'b1;
+#252;  meip_i=1'b1;*/
 end
 
+//this always block imitates an interrupt controller. uncomment if you are using machine external interrupt.
 /*
 always @(posedge clk_i)
 begin
 	if(irq_ack_o)
-		fast_irq_i = 1'b0;
+		meip_i = 1'b0;
 end*/
 
 endmodule
