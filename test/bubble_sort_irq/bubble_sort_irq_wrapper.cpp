ce54517b36ef040ae16c676e0808eafe90b83246
Yavuz Selim Tozlu <0yavuz0@gmail.com>
2021-07-17 23:47:26 +0300

    Update for wishbone

--- test/bubble_sort_irq/bubble_sort_irq_wrapper.cpp
+++ test/bubble_sort_irq/bubble_sort_irq_wrapper.cpp
@@ -1,12 +1,13 @@
 #include <verilated.h>
 #include <iostream>
 #include <fstream>
-#include "Vbarebones_top.h"
-#include "Vbarebones_top_memory_2rw__Ab.h"
-#include "Vbarebones_top_barebones_top.h"
+#include "Vbarebones_wb_top.h"
+#include "Vbarebones_wb_top_memory_2rw_wb__Ab.h"
+#include "Vbarebones_wb_top_memory_2rw__Ab_R800.h"
+#include "Vbarebones_wb_top_barebones_wb_top.h"
 #include "verilated_vcd_c.h"
 
-Vbarebones_top *barebones_top;
+Vbarebones_wb_top *barebones_wb_top;
 vluint64_t main_time = 0;
 
 int main(int argc, char** argv)
@@ -15,47 +16,51 @@ int main(int argc, char** argv)
     Verilated::commandArgs(argc, argv);
 	Verilated::traceEverOn(true);
 	VerilatedVcdC* tfp = new VerilatedVcdC;
-    barebones_top = new Vbarebones_top;
+    barebones_wb_top = new Vbarebones_wb_top;
 
     bin_file.seekg(0,bin_file.end);
     int len = bin_file.tellg();
     bin_file.seekg(0,bin_file.beg);
 
-    bin_file.read(reinterpret_cast<char*>(barebones_top->barebones_top->memory->mem),len);
+    bin_file.read(reinterpret_cast<char*>(barebones_wb_top->barebones_wb_top->memory->memory->mem),len);
 
-    barebones_top->trace(tfp, 99);
+    barebones_wb_top->trace(tfp, 99);
     tfp->open("simx.vcd");
-	barebones_top->reset_i = 0;
+	barebones_wb_top->reset_i = 1;
+    barebones_wb_top->eval();
+    tfp->dump(main_time);
+    main_time++;
+    barebones_wb_top->reset_i = 0;
     while (!Verilated::gotFinish())
     {
     	if (main_time > 10) {
-            barebones_top->reset_i = 1;
+            barebones_wb_top->reset_i = 1;
         }
         if ((main_time % 10) == 1) {
-            barebones_top->clk_i = 1;
+            barebones_wb_top->clk_i = 0;
         }
         if ((main_time % 10) == 6) {
-            barebones_top->clk_i = 0;
+            barebones_wb_top->clk_i = 1;
         }
-        if(barebones_top->irq_ack_o == 1)
-            barebones_top->meip_i = 0;
+        if(barebones_wb_top->irq_ack_o == 1)
+            barebones_wb_top->meip_i = 0;
 
         if(main_time == 1054)
-            barebones_top->meip_i = 1;
+            barebones_wb_top->meip_i = 1;
 
         if(main_time == 1150)
-            barebones_top->meip_i = 1;
+            barebones_wb_top->meip_i = 1;
 
         if(main_time == 2027)
-            barebones_top->meip_i = 1;
+            barebones_wb_top->meip_i = 1;
 
         if(main_time == 2403)
-            barebones_top->meip_i = 1;
+            barebones_wb_top->meip_i = 1;
 
         if(main_time == 2800)
-            barebones_top->meip_i = 1;                  
+            barebones_wb_top->meip_i = 1;                  
 
-		barebones_top->eval();
+		barebones_wb_top->eval();
         tfp->dump(main_time);
         main_time++;
         if(main_time > 50000)
@@ -65,8 +70,8 @@ int main(int argc, char** argv)
         }
     }
 
-    barebones_top->final();
+    barebones_wb_top->final();
     tfp->close();
-    delete barebones_top;
+    delete barebones_wb_top;
 }
 
