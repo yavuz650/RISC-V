e024dc3f86d6f1a4fe3b213c14f816bb2a58be5a
Yavuz Selim Tozlu <0yavuz0@gmail.com>
2021-07-18 00:23:15 +0300

    Added mtime peripheral

--- processor/barebones/barebones_wb_top.v
+++ processor/barebones/barebones_wb_top.v
@@ -1,12 +1,11 @@
-`timescale 1ns/1ps
-
 module barebones_wb_top(input clk_i,
                         input reset_i,
                         input meip_i,
                         input [15:0] fast_irq_i,
                         output irq_ack_o);
 
-parameter NUM_SLAVES = 3;
+parameter NUM_SLAVES = 4;
+wire mtip;
 
 //Wishbone master interface signals for core
 wire data_wb_cyc_o;
@@ -59,9 +58,11 @@ assign slave_adr_end[0] = 32'h0000_1DFF;
 assign slave_adr_begin[1] = 32'h0000_0000;
 assign slave_adr_end[1] = 32'h0000_1FFF;
 
-assign slave_adr_begin[2] = 32'h0000_2010;
-assign slave_adr_end[2] = 32'h0000_2010;
+assign slave_adr_begin[2] = 32'h0000_2000;
+assign slave_adr_end[2] = 32'h0000_200F;
 
+assign slave_adr_begin[3] = 32'h0000_2010;
+assign slave_adr_end[3] = 32'h0000_2010;
 
 assign wb_cyc_i[0] = inst_wb_cyc_o;
 assign wb_stb_i[0] = inst_wb_stb_o;
@@ -78,7 +79,7 @@ assign inst_wb_err_i = wb_err_o[0];
 
 genvar i;
 generate
-    for (i = 1; i<3 ;i=i+1)
+    for (i = 1; i<NUM_SLAVES ;i=i+1)
     begin
         assign wb_cyc_i[i] = data_wb_cyc_o;
         assign wb_stb_i[i] = data_wb_stb_o & ((slave_adr_begin[i] <= wb_adr_i[i]) && (wb_adr_i[i] <= slave_adr_end[i]));
@@ -92,7 +93,7 @@ generate
 endgenerate
 
 //Register strobe signals
-always @(posedge wb_clk_i[0] or posedge wb_rst_i[0]) 
+always @(posedge wb_clk_i[0] or posedge wb_rst_i[0])
 begin
     if(wb_rst_i[0])
         r_stb <= 0;
@@ -109,7 +110,7 @@ integer k;
 always @(*)
 begin
     valid = 1'b0;
-    for (k = 1; k<3 && valid!=1'b1; k=k+1)
+    for (k = 1; k < NUM_SLAVES && valid != 1'b1; k = k+1)
     begin
         if(r_stb[k])
         begin
@@ -133,12 +134,6 @@ assign data_wb_dat_i = r_data_wb_dat_i;
 assign data_wb_ack_i = r_data_wb_ack_i;
 assign data_wb_stall_i = r_data_wb_stall_i;
 assign data_wb_err_i = r_data_wb_err_i;
-
-/*
-assign data_wb_dat_i = mux_wb_dat_o[1] | mux_wb_dat_o[2];
-assign data_wb_ack_i = wb_ack_o[1] | wb_ack_o[2];
-assign data_wb_stall_i = wb_stall_o[1] | wb_stall_o[2];
-assign data_wb_err_i = wb_err_o[1] | wb_err_o[2];*/
 assign data_wb_clk_i = clk_i;
 assign data_wb_rst_i = ~reset_i;
 
@@ -175,7 +170,7 @@ core_wb core0 (.reset_i(reset_i),
 
                //Interrupts
                .meip_i(meip_i),
-               .mtip_i(1'b0),
+               .mtip_i(mtip),
                .msip_i(1'b0),
                .fast_irq_i(fast_irq_i),
                .irq_ack_o(irq_ack_o));
@@ -206,16 +201,33 @@ memory_2rw_wb #(.ADDR_WIDTH(11)) memory(.port0_wb_cyc_i(wb_cyc_i[0]),
                                         .port1_wb_rst_i(wb_rst_i[1]),
                                         .port1_wb_clk_i(wb_clk_i[1]));
 
-debug_interface_wb debug_if (.wb_cyc_i(wb_cyc_i[2]),
-                             .wb_stb_i(wb_stb_i[2]),
-                             .wb_we_i(wb_we_i[2]),
-                             .wb_adr_i(wb_adr_i[2]),
-                             .wb_dat_i(wb_dat_i[2]),
-                             .wb_sel_i(wb_sel_i[2]),
-                             .wb_stall_o(wb_stall_o[2]),
-                             .wb_ack_o(wb_ack_o[2]),
-                             .wb_dat_o(wb_dat_o[2]),
-                             .wb_err_o(wb_err_o[2]),
-                             .wb_rst_i(wb_rst_i[2]),
-                             .wb_clk_i(wb_clk_i[2]));
+mtime_registers_wb #(.mtime_adr(32'h0000_2000),
+                     .mtimecmp_adr(32'h0000_2008))
+                     mtime_regs(.wb_cyc_i(wb_cyc_i[2]),
+                                .wb_stb_i(wb_stb_i[2]),
+                                .wb_we_i(wb_we_i[2]),
+                                .wb_adr_i(wb_adr_i[2]),
+                                .wb_dat_i(wb_dat_i[2]),
+                                .wb_sel_i(wb_sel_i[2]),
+                                .wb_stall_o(wb_stall_o[2]),
+                                .wb_ack_o(wb_ack_o[2]),
+                                .wb_dat_o(wb_dat_o[2]),
+                                .wb_err_o(wb_err_o[2]),
+                                .wb_rst_i(wb_rst_i[2]),
+                                .wb_clk_i(wb_clk_i[2]),
+                                .mtip_o(mtip));
+
+debug_interface_wb debug_if (.wb_cyc_i(wb_cyc_i[3]),
+                             .wb_stb_i(wb_stb_i[3]),
+                             .wb_we_i(wb_we_i[3]),
+                             .wb_adr_i(wb_adr_i[3]),
+                             .wb_dat_i(wb_dat_i[3]),
+                             .wb_sel_i(wb_sel_i[3]),
+                             .wb_stall_o(wb_stall_o[3]),
+                             .wb_ack_o(wb_ack_o[3]),
+                             .wb_dat_o(wb_dat_o[3]),
+                             .wb_err_o(wb_err_o[3]),
+                             .wb_rst_i(wb_rst_i[3]),
+                             .wb_clk_i(wb_clk_i[3]));
+
 endmodule
